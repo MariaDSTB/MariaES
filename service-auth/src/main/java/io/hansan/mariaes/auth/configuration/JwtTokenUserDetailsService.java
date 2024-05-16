513863f2a58bb0e6519bd3152c1a37ecac662390
HanSanCoder <3313592503@qq.com>
2024-05-16 23:21:48 +0800

    feat: decoupled authentication and authorization

---
+++ service-auth/src/main/java/io/hansan/mariaes/auth/configuration/JwtTokenUserDetailsService.java
@@ -0,0 +1,9 @@
+package io.hansan.mariaes.auth.configuration;
+
+/**
+ * @author ：何汉叁
+ * @date ：2024/5/16 23:20
+ * @description：TODO
+ */
+public class JwtTokenUserDetailsService {
+}
---
+++ service-auth/src/test/java/io/hansan/mariaes/auth/ServiceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hansan.mariaes.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
